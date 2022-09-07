6f3b54b061040f869a44d58e074df2db561b2613
GB-Navarro <gbnavarro@usp.br>
2022-09-07 20:38:33 -0300

    feat: created the getPasswordByEmail function

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -3,7 +3,7 @@ import { users } from "@prisma/client";
 
 async function searchEmail(email: string) {
 
-    const result: users = await client.users.findFirst({
+    const result: users = await client.users.findUnique({
         where: {
             email: email
         }
@@ -21,6 +21,21 @@ async function insertUser(email: string, password: string) {
             password: password
         }
     })
+
+}
+
+async function getPasswordByEmail(email: string){
+    
+    const result: Omit<users, "id" | "email"> = await client.users.findUnique({
+        where:{
+            email: email
+        },
+        select:{
+            password: true
+        }
+    })
+
+    return result;
 
 }
 
