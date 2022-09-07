00e2be1f4b6e06ade442e6a557a49425cfcf40a8
GB-Navarro <gbnavarro@usp.br>
2022-09-07 17:36:02 -0300

    feat: added a errorHandler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/router.js"
+import errorHandler from "./middlewares/errorHandler.js";
+
+import "express-async-errors";
 
 dotenv.config({path:"../.env"});
 
@@ -10,6 +13,7 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(router);
+server.use(errorHandler);
 
 server.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express";
+
+export default async function errorHandler(error: any, req: Request, res: Response, next: NextFunction){
+    
+    if(error.code === "error_dataDontIsValid"){
+        return res.status(404).send({message: error.message})
+    }
+    
+    return res.sendStatus(500);
+}
