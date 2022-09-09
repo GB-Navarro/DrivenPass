d545adfcd3f96b9638f08daa6a6f8c63ea43caec
GB-Navarro <gbnavarro@usp.br>
2022-09-09 06:41:19 -0300

    \feat: created the credentialServices

---
+++ src/services/credentialServices.ts
@@ -0,0 +1,12 @@
+import { ICredentialData } from "../types/credentialTypes.js";
+
+async function create(data: ICredentialData){
+    
+}
+
+const credentialServices = {
+    
+    create
+}
+
+export default credentialServices;
