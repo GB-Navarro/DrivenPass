6c4440b4788381701f327b3dcb683ba2f16c5542
GB-Navarro <gbnavarro@usp.br>
2022-09-09 06:24:23 -0300

    feat: created the credentialMiddlewares and created the CredentialData interface

---
+++ src/middlewares/credentialMiddlewares.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express";
+import { IAuthData } from "../types/authTypes";
+
+
+
+const credentialMiddlewares = {
+
+}
+
+export default credentialMiddlewares;
