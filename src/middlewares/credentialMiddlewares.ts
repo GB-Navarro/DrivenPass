c62f80cc2c7334597951d74555905fc41afa2170
GB-Navarro <gbnavarro@usp.br>
2022-09-09 06:33:03 -0300

    feat: created the validateCreateSchema middleware

--- src/middlewares/credentialMiddlewares.ts
+++ src/middlewares/credentialMiddlewares.ts
@@ -1,10 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-import { IAuthData } from "../types/authTypes";
+import { ICredentialData } from "../types/credentialTypes";
 
+import credentialSchemas from "../schemas/credentialSchemas";
 
+function validateCreateSchema(req: Request, res: Response, next: NextFunction) {
+
+    const data: ICredentialData = req.body;
+    const result = credentialSchemas.create.validate(data);
+
+    if (result.error != undefined) {
+        throw { code: "error_dataDontIsValid", message: result.error.message };
+    }
+
+    next();
+}
 
 const credentialMiddlewares = {
-
+    
+    validateCreateSchema
 }
 
 export default credentialMiddlewares;
