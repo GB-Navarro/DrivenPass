d8ad969f95a093c332ef27fbc4db79e4e9afc068
GB-Navarro <gbnavarro@usp.br>
2022-09-10 21:20:08 -0300

    feat: created the checkTittleExistence function at the cardServices

--- src/services/cardServices.ts
+++ src/services/cardServices.ts
@@ -1,12 +1,14 @@
 import { ICardData } from "../types/cardTypes.js";
 import { Tittle } from "../types/genericTypes.js";
+
+import cardRepository from "../repositories/cardRepository.js";
 async function checkTittleExistence(tittle: string, userId: number) {
 
-    //const result: Tittle = await cardRepository.getTittleById(tittle, userId);
+    const result: Tittle = await cardRepository.getTittleById(tittle, userId);
 
-    /*if (result != null) {
+    if (result != null) {
         throw { code: "error_thisTittleAlreadyExist", message: "This tittle already exist!" };
-    }*/
+    }
 }
 
 async function create(userId: number, cardData: ICardData){
