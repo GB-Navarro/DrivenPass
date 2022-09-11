d26a9cf538f3e0cb2716d8337f3b31e7922cea0d
GB-Navarro <gbnavarro@usp.br>
2022-09-11 10:35:47 -0300

    commiting before change branch

--- src/controllers/documentController.ts
+++ src/controllers/documentController.ts
@@ -1,15 +1,22 @@
 import { Request, Response } from "express";
 import { IUserData } from "../types/authTypes.js";
+import { IDocumentData } from "../types/documentTypes.js";
+
+import documentServices from "../services/documentServices.js";
 
 async function create(req: Request, res: Response ){
 
     const { id: userId }: Omit<IUserData, "email"> = res.locals.data;
 
+    const documentData: IDocumentData = req.body;
+
+    await documentServices.create(userId, documentData);
+
     res.status(200).send("The document has been created!");
 }
 
 const documentController = {
-    
+
     create
 }
 
