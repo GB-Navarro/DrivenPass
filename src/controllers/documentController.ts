2e3baeb846c795216c36f55f5cd3d22590535bb9
GB-Navarro <gbnavarro@usp.br>
2022-09-11 10:38:29 -0300

    feat: the routes/controllers structure to search a document has been created

--- src/controllers/documentController.ts
+++ src/controllers/documentController.ts
@@ -8,9 +8,31 @@ async function create(req: Request, res: Response ){
     res.status(200).send("The document has been created!");
 }
 
+async function search(req: Request, res: Response) {
+
+    const { id: userId }: Omit<IUserData, "email"> = res.locals.data;
+
+    //const documentsData: documents[] = await documentServices.search(userId);
+
+    //res.status(200).send(documentsData);
+}
+
+async function searchById(req: Request, res: Response) {
+
+    const { id: userId }: Omit<IUserData, "email"> = res.locals.data;
+
+    const { id: documentId } = req.params;
+
+    //const document = await documentServices.searchById(userId, parseInt(documentId));
+
+    //res.status(200).send(document);
+}
+
 const documentController = {
     
-    create
+    create,
+    search,
+    searchById
 }
 
 export default documentController;
--- src/routes/documentRoutes.ts
+++ src/routes/documentRoutes.ts
@@ -7,5 +7,7 @@ import genericMiddlewares from "../middlewares/genericMiddlewares.js";
 const documentRouter = Router();
 
 documentRouter.post("/documents/create", genericMiddlewares.validateToken, documentMiddlewares.validateCreateSchema ,documentController.create);
+documentRouter.get("/documents/search", genericMiddlewares.validateToken ,documentController.search);
+documentRouter.get("/documents/search/:id", genericMiddlewares.validateToken ,documentController.searchById);
 
 export default documentRouter;
