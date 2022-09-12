ae5640534c509827e7991bc3b4bfb3bdbde7fb61
GB-Navarro <gbnavarro@usp.br>
2022-09-12 16:53:07 -0300

    fix: fixed a status code response

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,14 +2,14 @@ import { Request, Response, NextFunction } from "express";
 
 export default async function errorHandler(error: any, req: Request, res: Response, next: NextFunction) {
 
+    if (error.code === "error_wrongPassword" || error.code === "error_notReceivedAToken" || error.code === "error_invalidToken") {
+        return res.status(401).send(error.message);
+    }
     if (error.code === "error_dataDontIsValid" || error.code === "error_thisEmailIsNotRegistered" || error.code === "error_InvalidRequest") {
         return res.status(404).send(error.message);
     }
-    if (error.code === "error_emailAlreadyInUse" || "error_thisTittleAlreadyExist") {
+    if (error.code === "error_emailAlreadyInUse" || error.code === "error_thisTittleAlreadyExist") {
         return res.status(409).send(error.message);
-    }
-    if (error.code === "error_wrongPassword" || error.code === "error_notReceivedAToken" || error.code === "error_invalidToken") {
-        return res.status(401).send(error.message);
     }
 
     return res.sendStatus(500);
